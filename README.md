ab15dc5937adf2b10ad84abcb6d948db624d723e
Alexander Zinovev <a.zinovev@hotmail.com>
2023-07-29 15:31:52 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# blog_plus_db
---
+++ main.py
@@ -0,0 +1,80 @@
+from flask import Flask, render_template, redirect, url_for, jsonify
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+from datetime import date
+import json
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+# CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy()
+db.init_app(app)
+
+
+# CONFIGURE TABLE
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+    def to_dict(self):
+        posts_dict = {}
+        for column in self.__table__.columns:
+            posts_dict[column.name] = getattr(self, column.name)
+        return posts_dict
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+def get_all_posts():
+    result = db.session.execute(db.select(BlogPost))
+    posts = result.scalars().all()
+    # blog_posts = result.scalars().all()
+    # posts = [blog_post.to_dict() for blog_post in blog_posts]
+    # print("✅", json.dumps(posts, indent=2))
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route('/post/<post_id>')
+def show_post(post_id):
+    # TODO: Retrieve a BlogPost from the database based on the post_id
+    # result = db.session.execute(db.select(BlogPost).where(BlogPost.id == post_id))
+    # requested_post = result.scalar_one()
+    requested_post = db.get_or_404(BlogPost, post_id)
+    return render_template("post.html", post=requested_post)
+
+
+# TODO: add_new_post() to create a new blog post
+
+# TODO: edit_post() to change an existing blog post
+
+# TODO: delete_post() to remove a blog post from the database
+
+# Below is the code from previous lessons. No changes needed.
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5003)
